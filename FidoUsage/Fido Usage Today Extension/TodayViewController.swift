0810fddb41523f3e90072f5abfea0b3cca8413d8
Honghao Zhang <zhh358@gmail.com>
2015-09-24 09:46:30 -0400

    Updated today extension

--- FidoUsage/Fido Usage Today Extension/TodayViewController.swift
+++ FidoUsage/Fido Usage Today Extension/TodayViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 import NotificationCenter
+import Client
+import Loggerithm
+
+let log = Loggerithm()
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+	@IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+		
+		log.debug("haha")
+		
+		let client = Client.sharedInstance
+		let number = "5197812862"
+		let password = "Zhh358279765099"
+		
+		log.info("Login with number: \(number)")
+		client.loginWithNumber(number, password: password) { [unowned self] (succeed, resultDict) in
+			if succeed {
+				log.debug("Login Results: \(resultDict)")
+				
+				self.helloLabel.text = resultDict!["Account holder"]
+				
+				log.info("Goto View Usage Page ...")
+				client.gotoViewUsagePage { (succeed, sections) in
+					guard let sections = sections else {
+						log.error("Sections are emtpy")
+						return
+					}
+					log.debug("Usage Sections: \(sections)")
+				}
+			} else {
+				log.error("")
+			}
+		}
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
